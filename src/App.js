71387796486326e5edaafe5f2426a8b42433243b
Akshat Atray <akshatatray31@gmail.com>
2021-08-05 19:13:46 +0530

    Basic App Setup 🛠

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+
 const App = () => {
   return (
-    <div>
-      App!
-    </div>
+    <Router>
+      <Route path="/" exact component={Home} />
+    </Router>
   );
 }
 
---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Navbar from '../../components/Navbar';
+import MobileNavbar from '../../components/MobileNavbar';
+
+const Home = () => {
+    return (
+        <div className="Home">
+            <Navbar />
+            <MobileNavbar />
+        </div>
+    )
+}
+
+export default Home;
